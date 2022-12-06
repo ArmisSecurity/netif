48e0cd27ba96d313c1a75b751be6af640dd0b9b2
hagar ishay <hagar.ishay@armis.com>
2022-12-06 13:57:48 +0200

    reorder

--- interfacesReader.go
+++ interfacesReader.go
@@ -163,10 +163,10 @@ func (ir *InterfacesReader) parseDetails(line string, iface *string) error {
 		sline[0] != "vendor" &&
 		sline[0] != "client" &&
 		sline[0] != "dns-nameservers" && // options
-		sline[0] != "wpa-ssid" && // options
-		sline[0] != "wpa-psk" && // options
 		sline[0] != "dns-domain" &&
 		sline[0] != "dns-search" &&
+		sline[0] != "wpa-ssid" && // options
+		sline[0] != "wpa-psk" && // options
 		sline[0] != "up" &&
 		sline[0] != "pre-up" &&
 		sline[0] != "post-up" &&
